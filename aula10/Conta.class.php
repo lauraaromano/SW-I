2fa1185c1f38586c2da8bcd4b060c8d2b8b50921
lauraaromano <161627881+lauraaromano@users.noreply.github.com>
2025-05-16 18:31:11 -0300

    Add files via upload

---
+++ aula10/Conta.class.php
@@ -0,0 +1,38 @@
+<?php
+    class Conta{
+        public $Nome;
+        public $Cpf;
+        // private $Saldo;
+
+        public function Sacar($valor){
+
+            if($valor > $this->Saldo ){
+                return "Saldo insuficiente!";
+            }else{
+                $this->Saldo = $this->Saldo - $valor;
+                $texto = "Op Realizado com sucesso. Saldo Atual é R$". $this->Saldo;
+                $this->Saldo;
+                return $texto;
+            }
+
+
+            $this->Saldo = $this->Saldo - $valor;
+            // $this->Saldo -= $valor;
+            $texto = "Op Realizado com sucesso. Saldo Atual é R$". $this->Saldo;
+            return $texto;
+
+        }
+
+        public function Depositar($valor){
+            $this->Saldo = $this->Saldo + $valor;
+            $texto = "Op Realizado com sucesso. Saldo Atual é R$". $this->Saldo;
+            // $this->Saldo += $valor;
+            return $texto;
+        }
+        public function ConsultarSaldo(){
+            echo "NOME: ". $this->Nome . "<br>";
+            echo "CPF: ". $this->Cpf . "<br>";
+            echo "SALDO: ". $this->Saldo . "<br>";
+        }
+    }
+?>
