2a95212c32b914e7fd1fcf5f05b179a40148031c
lauraaromano <161627881+lauraaromano@users.noreply.github.com>
2025-06-11 14:29:04 -0300

    Add files via upload

---
+++ aula13/Aluno.class.php
@@ -0,0 +1,41 @@
+<?php
+    class Aluno{
+        private $Nome;
+        private $Nota1;
+        private $Nota2;
+
+        public function __construct($Nome,$n1,$n2){
+            $this -> Nome = $Nome;
+            $this -> Nota1 = $n1;
+            $this -> Nota2 = $n2;
+        }
+
+        public function getNome(){
+            return $this->Nome;
+        }
+
+        public function getNota1(){
+            return $this->Nota1;
+        }
+
+        public function getNota2(){
+            return $this->Nota2;
+        }
+
+        public function Media($n1,$n2){
+            $media = ($n1 + $n2)/2;
+            return $media;
+        }
+
+        public function VerificarSituacao($media){
+            if($media>=7){
+                //APROVADO
+                return "APROVADO";
+
+            }else{
+                //REPROVADO
+                return "REPROVADO";
+            }
+        }
+    }
+?>
---
+++ aula13/usaaluno.php
@@ -0,0 +1,15 @@
+<?php
+    include_once 'Aluno.class.php';
+
+    $fulano = new Aluno("Fulano da Silva",5,7);
+
+    $nota1 = $fulano->getNota1();
+    $nota2 = $fulano->getNota2();
+    
+    // echo $nota1;
+    $aluno = $fulano->
+
+    $media = $fulano->Media($nota1,$nota2);
+
+    echo $fulano->VerificarSituacao($media);
+?>
